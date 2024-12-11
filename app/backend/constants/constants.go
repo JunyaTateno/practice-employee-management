fa40c3e30ceb0d9c2ba82710b593931be7788561
JunyaTateno <gilgame.kefka56@gmail.com>
2024-12-12 00:23:41 +0900

    [add] バックエンドの実装 -- 定数定義用のconstants.goを追加した。

---
+++ app/backend/constants/constants.go
@@ -0,0 +1,10 @@
+// 定数定義
+package constants
+
+const (
+	DBUser     = "youruser"     // DBユーザー名
+	DBPassword = "yourpassword" // DBパスワード
+	DBHost     = "db"           // DBホスト名（Dockerコンテナ名）
+	DBPort     = "3306"         // DBポート番号
+	DBName     = "company"      // 使用するDB名
+)
