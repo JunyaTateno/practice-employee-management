68ee056ad1bbe7e29b794230b29a60935f2bf2b9
JunyaTateno <gilgame.kefka56@gmail.com>
2025-01-08 19:27:13 +0900

    [add] バックエンドの実装 -- ルーティングを定義したファイルを追加した。

---
+++ app/backend/routes/routes.go
@@ -0,0 +1,24 @@
+// ルーティングを定義するファイル
+package routes
+
+import (
+	"employee-management/controllers"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// マルチプレクサにエンドポイントとハンドラを登録する関数
+func RegisterRoutes() *mux.Router {
+	// 新しいルーターを作成
+	router := mux.NewRouter()
+
+	// エンドポイントとそのハンドラを登録
+	router.HandleFunc("/employees", controllers.GetAllEmployees).Methods(http.MethodGet) // 社員情報の全取得
+	router.HandleFunc("/employees", controllers.AddEmployee).Methods(http.MethodPost)    // 新しい社員を登録
+
+	// 他のエンドポイントをここに追加可能
+	// 例: router.HandleFunc("/employees/{id}", controllers.UpdateEmployee).Methods(http.MethodPut)
+
+	return router
+}
