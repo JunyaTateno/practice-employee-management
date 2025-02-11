8ec559fa06e712412f0426f206f3c5912bbe63c8
JunyaTateno <gilgame.kefka56@gmail.com>
2025-02-12 01:47:51 +0900

    [update] API周りの改善 - constants.goの中身を整理し、DB設定変数はdb.goに移動した。

--- app/backend/constants/constants.go
+++ app/backend/constants/constants.go
@@ -1,24 +1,6 @@
 // 定数定義
 package constants
 
-import "os"
-
-var (
-	DBUser     = getEnv("DB_USER", "youruser")         // DBユーザー名
-	DBPassword = getEnv("DB_PASSWORD", "yourpassword") // DBパスワード
-	DBHost     = getEnv("DB_HOST", "db")               // DBホスト名
-	DBPort     = getEnv("DB_PORT", "3306")             // DBポート番号
-	DBName     = getEnv("DB_NAME", "company")          // 使用するDB名
-)
-
-// 環境変数を取得するヘルパー関数
-func getEnv(key, defaultValue string) string {
-	if value, exists := os.LookupEnv(key); exists {
-		return value
-	}
-	return defaultValue
-}
-
 // CORSで許可するオリジン
 var AllowedOrigins = map[string]bool{
 	"http://localhost":                 true, // 開発環境 (Nginx)
